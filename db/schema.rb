cf22297bc32c6e34e9b591b618c8e35ced642e7b
vagrant <vagrant@precise32.(none)>
2014-07-06 02:31:28 +0000

    added user scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140706022533) do
+ActiveRecord::Schema.define(version: 20140706023028) do
 
   create_table "courses", force: true do |t|
     t.string   "title"
     t.integer  "task_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
