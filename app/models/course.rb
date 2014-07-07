abd6ce96c2c8690ca41acdcbf6977621f70f85d8
vagrant <vagrant@precise32.(none)>
2014-07-07 15:55:53 +0000

    AddTypeToUsers and generate devise:views

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  task_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Course < ActiveRecord::Base
 end
--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  task_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'test_helper'
 
 class CourseTest < ActiveSupport::TestCase
